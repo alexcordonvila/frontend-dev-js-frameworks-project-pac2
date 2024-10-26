1caca0fc3529c01a970904a80bd2b802c71793b5
alexcordonvila <alexcordonvila@gmail.com>
2024-10-26 19:41:21 +0200

    Afegir comentaris ejer1-b

--- PEC2_Ej1/ejer1-c.js
+++ PEC2_Ej1/ejer1-c.js
@@ -1,14 +1,8 @@
-//Implementació del mètode findOne que rep l'array on buscar i l'objecte a buscar per clau-valor
 const findOne = (list, { key, value }) => {
-  //retornem una Promise un cop es resol l'execució del timeout
   return new Promise((resolve, reject) => {
-    //emula un comportament asíncron amb un delay de 2000ms (2segons)
     setTimeout(() => {
-        //cerca de coincidència, usant la funció find, entre "valor" i el contingut de l'array
         const element = list.find((element) => element[key] === value);
-        //si l'element a cercar existeix dins la llista retornem resolve sino el reject amb un msg d'error
         element ? resolve(element) : reject({ msg: 'ERROR: Element Not Found' });
-        //delay del setTimeout en ms
       }, 2000);
     });
 };
@@ -23,15 +17,12 @@ const users = [
     rol: "Boss",
   },
 ];
-//Print per consola de manera síncrona
 console.log("findOne success");
-//consum de la funció findOne usant la promise fent una cerca que es resol favorablement.
 findOne(users, { key: 'name', value: 'Carlos' })
   .then((element) => console.log(`user: ${element.name}`)) 
   .catch((error) => console.log(error.msg));
-//Print per consola de manera síncrona
+
 console.log("findOne error");
-//consum de la funció findOne usant la promise fent una cerca que es resol desfavorablement.
 findOne(users, { key: 'name', value: 'Fermin' })
   .then((element) => console.log(`user: ${element.name}`)) 
   .catch((error) => console.log(error.msg));
--- README.md
+++ README.md
@@ -9,6 +9,7 @@
 #### **Breu descripció:** 
     a) En aquest exercici, ha calgut identificar els motius del resultat d'executar el codi. Cal entendre el fil d'execució d'un codi que combina sincronia i asincronia.
     b) En aquest segon apartat s'ha refactoritzat el codi per a deixar d'usar callbacks i substituir-ho per promises.
+    c)
 
 
 • TODO: Breu descripció del que s'ha realitzat en aquesta PAC, dificultats, millores realitzades, si cal tenir alguna cosa en compte a l'hora de corregir/executar la pràctica o qualsevol aspecte que vulgueu destacar.
