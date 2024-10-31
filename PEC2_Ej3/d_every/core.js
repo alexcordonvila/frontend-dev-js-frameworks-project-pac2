e9e689461d1c8f1bfa1825f82ff642f7cd6456df
alexcordonvila <alexcordonvila@gmail.com>
2024-10-31 21:58:53 +0100

    Implementar d_every/core.js

--- PEC2_Ej3/d_every/core.js
+++ PEC2_Ej3/d_every/core.js
@@ -2,14 +2,14 @@
 // are even numbers.
 
 function allEven(input) {
-  return input;
+  return input.every(num => num %2 === 0);
 }
 
 // Check to see if all elements in an array
 // are of the same type.
 
 function allSameType(input) {
-  return input;
+  return input.every(element => typeof element === typeof input[0]);
 }
 
 // Check to see if every element in the matrix is
@@ -17,14 +17,17 @@ function allSameType(input) {
 // greater than 0.
 
 function positiveMatrix(input) {
-  return input;
+  return input.every(element => Array.isArray(element) && element.every(num => num > 0));
 }
 
 // Check that all items in an array are strings
 // and that they all only contain the same vowels.
 
 function allSameVowels(input) {
-  return input;
+  return input.every(word =>{ 
+    const vowels = word.match(/[aeiouAEIOU]/g); 
+    return typeof word === "string" && vowels.every(vowel => vowel === vowels[0]);
+  });
 }
 
 module.exports = {
