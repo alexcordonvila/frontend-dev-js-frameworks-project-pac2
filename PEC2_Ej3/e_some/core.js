66b8c2f64cdf8d5c38ff7ed57e212fc8a520506b
alexcordonvila <alexcordonvila@gmail.com>
2024-11-01 02:22:26 +0100

    Implementar e_some/core.js

--- PEC2_Ej3/e_some/core.js
+++ PEC2_Ej3/e_some/core.js
@@ -2,28 +2,28 @@
 // array are numbers greater than 10.
 
 function anyGreaterThan10 (input) {
-  return input;
+  return input.some(num => num > 10);
 };
 
 // Check to see if any of the strings in
 // the array is longer than 10 characters.
 
 function longWord (input) {
-  return input;
+  return input.some(words=>words.length>10);
 };
 
 // Check to see if any of the elements in
 // the matrix are true.
 
 function truePossibilities (input) {
-  return input;
+  return input.flat().some(elem=>elem === true);
 };
 
 // Check to see if 'Lost' is in
 // the phrase (using some).
 
 function lostCarcosa (input) {
-  return input;
+  return input.some(phrase => phrase.split(" ").some(word => word === "Lost"));
 };
 
 module.exports = {
